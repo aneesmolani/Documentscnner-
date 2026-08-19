e7425a3bf038faad7fb6ce90905e1cdef1f13b91
aneesmolani <135018266+aneesmolani@users.noreply.github.com>
2026-08-19 14:03:38 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.13.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    // Kotlin 2.0+ के लिए कंपोज कंपाइलर प्लगइन
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
 }
+
